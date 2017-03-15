386c61a9036fdb31dda1c7eddcac1ea4b5789f4e
Dosenpfand <m@sad.bz>
2017-03-15 18:42:35 +0100

    add main sim

--- manhattan_pathloss.py
+++ manhattan_pathloss.py
@@ -16,7 +16,7 @@ def gen_streets_and_vehicles(lam_s, lam_v, road_len):
     counts_veh = np.zeros(count_streets, dtype=int)
     for i_street in np.arange(count_streets):
         while counts_veh[i_street] == 0:
-            counts_veh[i_street] = np.random.poisson(lam_s * road_len, 1)
+            counts_veh[i_street] = np.random.poisson(lam_v * road_len, 1)
     count_veh_all = np.sum(counts_veh)
     coords_veh_x = np.random.uniform(0, road_len, count_veh_all)
     coords_veh_y = np.repeat(coords_streets, counts_veh)
@@ -42,66 +42,122 @@ def find_same_street_veh(coords_veh, coords_own):
     return ind_veh_same_street
 
 
-# def find_olos_veh(coords_same_street, coords_own):
-#     dir_own = int(coords_own[2])
+def find_los_veh(coords_same_street, coords_own):
+    """Finds the vehicles that have a line of sight to the own vehicle"""
+    dir_own = int(coords_own[2])
+    ind_diff = 1-dir_own
+    distances = coords_own[ind_diff] - coords_same_street[:, ind_diff]
+    ind_distances_pos = np.flatnonzero(distances > 0)
+    distances_pos = distances[ind_distances_pos]
+    ind_los_pos = np.argmin(distances_pos)
+    ind_distances_neg = np.flatnonzero(distances < 0)
+    distances_neg = distances[ind_distances_neg]
+    ind_los_neg = np.argmax(distances_neg)
+    return ind_distances_pos[ind_los_pos], ind_distances_neg[ind_los_neg]
 
+def find_nlos_veh(coords_other_streets, coords_own):
+    """Finds vehicles that have non line of sight to the own vehicle"""
+    dir_own = int(coords_own[2])
+    ind_nlos = np.flatnonzero(coords_other_streets[:, 2] != dir_own)
+    return ind_nlos
 
-if __name__ == '__main__':
+def main_sim():
+    """Main simulation function"""
     # configuration
-    LAM_S = 3e-3
-    LAM_V = 1e-2
-    ROAD_LEN = 1e4
-    PL_THR = -110
+    lam_s = 2e-3
+    lam_v = 2e-3
+    road_len = 1e4
+    pl_threshold = -110
 
     # Street and vehicle generation
-    COORDS_VEH_X = gen_streets_and_vehicles(LAM_S, LAM_V, ROAD_LEN)
-    COORDS_VEH_X = np.column_stack((COORDS_VEH_X, np.ones(np.shape(COORDS_VEH_X)[0])))
-    COORDS_VEH_Y = np.fliplr(gen_streets_and_vehicles(LAM_S, LAM_V, ROAD_LEN))
-    COORDS_VEH_Y = np.column_stack((COORDS_VEH_Y, np.zeros(np.shape(COORDS_VEH_Y)[0])))
-    COORDS_VEH = np.vstack((COORDS_VEH_X, COORDS_VEH_Y))
-    INDEX_OWN = find_own_veh(ROAD_LEN, COORDS_VEH[:, 0:2])
-    COORDS_OWN = COORDS_VEH[INDEX_OWN, :]
-    COORDS_VEH = np.delete(COORDS_VEH, INDEX_OWN, axis=0)
+    coords_veh_x = gen_streets_and_vehicles(lam_s, lam_v, road_len)
+    coords_veh_x = np.column_stack((coords_veh_x, np.ones(np.shape(coords_veh_x)[0])))
+    coords_veh_y = np.fliplr(gen_streets_and_vehicles(lam_s, lam_v, road_len))
+    coords_veh_y = np.column_stack((coords_veh_y, np.zeros(np.shape(coords_veh_y)[0])))
+    coords_veh = np.vstack((coords_veh_x, coords_veh_y))
+    ind_own = find_own_veh(road_len, coords_veh[:, 0:2])
+    coords_own = coords_veh[ind_own, :]
+    coords_veh = np.delete(coords_veh, ind_own, axis=0)
+
+    # Determine LOS/OLOS/NLOS
+    ind_same_street = find_same_street_veh(coords_veh, coords_own)
+    ind_other_streets = np.setdiff1d(np.arange(np.shape(coords_veh)[0]), ind_same_street)
+    coords_same_street = coords_veh[ind_same_street, :]
+    coords_other_streets = coords_veh[ind_other_streets, :]
+    ind_los = np.asarray(find_los_veh(coords_same_street, coords_own))
+    ind_olos = np.setdiff1d(np.arange(np.shape(coords_same_street)[0]), ind_los)
+    coords_los = coords_same_street[ind_los, :]
+    coords_olos = coords_same_street[ind_olos, :]
+    ind_nlos = find_nlos_veh(coords_other_streets, coords_own)
+    ind_very_high_pl = np.setdiff1d(np.arange(np.shape(coords_other_streets)[0]), ind_nlos)
+    coords_nlos = coords_other_streets[ind_nlos, :]
+    coords_very_high_pl = coords_other_streets[ind_very_high_pl, :]
 
     # Pathloss calculation
-    DISTANCES = np.linalg.norm(COORDS_VEH[:, 0:2] - COORDS_OWN[0:2], ord=2, axis=1)
-    PL = pathloss.Pathloss()
-    PATHLOSSES_LOS = PL.pathloss_los(DISTANCES)
-    COORDS_IN_RANGE_IND = np.flatnonzero(PATHLOSSES_LOS > PL_THR)
-    COORDS_IN_RANGE = COORDS_VEH[COORDS_IN_RANGE_IND, :]
-    COORDS_NOT_IN_RANGE = np.delete(COORDS_VEH, COORDS_IN_RANGE_IND, axis=0)
+    # TODO optimize
+    distances_same_street = np.linalg.norm(coords_same_street[:, 0:2] - coords_own[0:2], ord=2, \
+        axis=1)
+    # TODO: own vehicle assumed to be receiver
+    dir_own = int(coords_own[2])
+    distances_nlos_rx = np.abs(coords_nlos[:, 1-dir_own] - coords_own[1-dir_own])
+    distances_nlos_tx = np.abs(coords_nlos[:, dir_own] - coords_own[dir_own])
+    pl_obj = pathloss.Pathloss()
 
-    # Plot same street vehicles
-    IND_SAME_STREET = find_same_street_veh(COORDS_VEH, COORDS_OWN)
-    COORDS_SAME_STREET = COORDS_VEH[IND_SAME_STREET, :]
-    COORDS_NOT_SAME_STREET = np.delete(COORDS_VEH, IND_SAME_STREET, axis=0)
-    plt.figure()
-    plt.scatter(COORDS_NOT_SAME_STREET[:, 0], COORDS_NOT_SAME_STREET[:, 1],
-                label='Not same street')
-    plt.scatter(COORDS_SAME_STREET[:, 0], COORDS_SAME_STREET[:, 1],
-                label='Same street')
-    plt.scatter(COORDS_OWN[0], COORDS_OWN[1], label='Own vehicle')
+    # TODO: minus for both functions?
+    pathlosses_los = -pl_obj.pathloss_los(distances_same_street[ind_los])
+    pathlosses_olos = -pl_obj.pathloss_olos(distances_same_street[ind_olos])
+    pathlosses_nlos = pl_obj.pathloss_nlos(distances_nlos_rx, distances_nlos_tx)
+    # TODO: infinity?
+    pathlosses_very_high_pl = np.inf*np.ones_like(ind_very_high_pl)
+
+    pathlosses = np.zeros(np.shape(coords_veh)[0])
+    pathlosses[ind_same_street[ind_los]] = pathlosses_los
+    pathlosses[ind_same_street[ind_olos]] = pathlosses_olos
+    pathlosses[ind_other_streets[ind_nlos]] = pathlosses_nlos
+    # pathlosses[ind_other_streets[ind_very_high_pl]] = pathlosses_very_high_pl
+    # TODO: !
+    pathlosses[ind_other_streets[ind_very_high_pl]] = max(pathlosses) + 50
+
+    ind_in_range = np.flatnonzero(pathlosses > pl_threshold)
+    coords_in_range = coords_veh[ind_in_range, :]
+    coords_out_range = np.delete(coords_veh, ind_in_range, axis=0)
+
+    # Plot pathloss
+    fig, ax = plt.subplots()
+    cax = plt.scatter(coords_veh[:, 0], coords_veh[:, 1], marker='x', c=pathlosses, \
+        cmap=plt.cm.magma, rlabel='Other vehicles')
+    
+    plt.scatter(coords_own[0], coords_own[1], c='black', marker='o', label='Own vehicle')
+    ax.set_title('Vehicle positions and pathloss')
+    plt.xlabel('X coordinate [m]')
+    plt.ylabel('Y coordinate [m]')
+
+    pl_min = np.min(pathlosses)
+    pl_max = np.max(pathlosses)
+    pl_med = np.mean((pl_min, pl_max))
+
+    string_min = '{:.0f}'.format(pl_min)
+    string_med = '{:.0f}'.format(pl_med)
+    string_max = '{:.0f}'.format(pl_max)
+
+    cbar = fig.colorbar(cax, ticks=[pl_min, pl_med, pl_max], orientation='horizontal')
+    cbar.ax.set_xticklabels([string_min, string_med, string_max])
+    cbar.ax.set_xlabel('Pathloss [dB]')
     plt.legend()
     plt.grid(True)
 
-    # Plot x and y vehicles
-    COORDS_X_IND = np.flatnonzero(COORDS_VEH[:, 2] == 1)
-    COORDS_Y_IND = np.flatnonzero(COORDS_VEH[:, 2] == 0)
-    plt.figure()
-    plt.scatter(COORDS_VEH[COORDS_X_IND, 0], COORDS_VEH[COORDS_X_IND, 1],
-                label='Traveling in x direction')
-    plt.scatter(COORDS_VEH[COORDS_Y_IND, 0], COORDS_VEH[COORDS_Y_IND, 1],
-                label='Traveling in y direction')
-    plt.legend()
-    plt.grid(True)
+    # # Plot LOS/OLOS/NLOS
+    # plt.figure()
+    # plt.scatter(coords_very_high_pl[:, 0], coords_very_high_pl[:, 1], label='Very high PL')
+    # plt.scatter(coords_nlos[:, 0], coords_nlos[:, 1], label='NLOS')
+    # plt.scatter(coords_olos[:, 0], coords_olos[:, 1], label='OLOS')
+    # plt.scatter(coords_los[:, 0], coords_los[:, 1], label='LOS')
+    # plt.scatter(coords_own[0], coords_own[1], label='Own vehicle')
+    # plt.legend()
+    # plt.grid(True)
 
-    # Plot vehicles in range
-    plt.figure()
-    plt.scatter(COORDS_NOT_IN_RANGE[:, 0], COORDS_NOT_IN_RANGE[:, 1], label='Not in range')
-    plt.scatter(COORDS_IN_RANGE[:, 0], COORDS_IN_RANGE[:, 1], label='In range')
-    plt.scatter(COORDS_OWN[0], COORDS_OWN[1], label='Own vehicle')
-    plt.legend()
-    plt.grid(True)
+    # Show plots
     plt.show()
 
-
+if __name__ == '__main__':
+    main_sim()
