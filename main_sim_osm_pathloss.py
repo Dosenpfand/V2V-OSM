d5b3c42f62138a62efc54307834587c64c85bb01
Dosenpfand <m@sad.bz>
2017-04-03 12:24:43 +0200

    refactor

--- main_sim_osm_pathloss.py
+++ main_sim_osm_pathloss.py
@@ -1,14 +1,12 @@
 """ Generates streets, buildings and vehicles from OpenStreetMap data with osmnx"""
 
 # Standard imports
-import argparse
 import os.path
 import ipdb
 
 # Extension imports
 import numpy as np
 import matplotlib.pyplot as plt
-import networkx as nx
 
 # Local imports
 import pathloss
@@ -16,93 +14,9 @@ import plot
 import utils
 import osmnx_addons as ox_a
 import geometry as geom_o
-import vehicle_distribution as dist
+import vehicles
 import propagation as prop
 
-class Vehicles:
-    # TODO: only points as attributes and get coordinates from points when requested?
-
-    def __init__(self, points, graphs=None):
-        self.points = points
-        self.coordinates = geom_o.extract_point_array(points)
-        self.graphs = graphs
-        self.pathlosses = np.zeros(np.size(points))
-        self.distances = np.zeros(np.size(points))
-        self.idxs = {}
-
-    def add_key(self, key, value):
-        self.idxs[key] = value
-
-    def get(self, key=None):
-        if key is None:
-            return self.coordinates
-        else:
-            return self.coordinates[self.idxs[key]]
-
-    def get_points(self, key=None):
-        if key is None:
-            return self.points
-        else:
-            return self.points[self.idxs[key]]
-
-    def get_graph(self, key=None):
-        if key is None:
-            return self.graphs
-        else:
-            return self.graphs[self.idxs[key]]
-
-    def get_idxs(self, key):
-        return self.idxs[key]
-
-    def set_pathlosses(self, key, values):
-        self.pathlosses[self.idxs[key]] = values
-
-    def get_pathlosses(self, key=None):
-        if key is None:
-            return self.pathlosses
-        else:
-            return self.pathlosses[self.idxs[key]]
-
-    def set_distances(self, key, values):
-        self.distances[self.idxs[key]] = values
-
-    def get_distances(self, key=None):
-        if key is None:
-            return self.distances
-        else:
-            return self.distances[self.idxs[key]]
-
-def generate_vehs(graph_streets, street_idxs):
-
-    count_veh = np.size(street_idxs)
-    points_vehs = np.zeros(count_veh, dtype=object)
-    graphs_vehs = np.zeros(count_veh, dtype=object)
-
-    for iteration, index in enumerate(street_idxs):
-        street = graph_streets.edges(data=True)[index]
-        street_geom = street[2]['geometry']
-        point_veh = dist.choose_random_point(street_geom)
-        points_vehs[iteration] = point_veh[0]
-        # NOTE: All vehicle nodes get the prefix 'v'
-        node = 'v' + str(iteration)
-        # Add vehicle, needed intersections and edges to graph
-        graph_iter = nx.MultiGraph(node_veh=node)
-        node_attr = {'geometry': point_veh[0], 'x' : point_veh[0].x, 'y' : point_veh[0].y}
-        graph_iter.add_node(node, attr_dict=node_attr)
-        graph_iter.add_nodes_from(street[0:2])
-
-        # Determine street parts that connect vehicle to intersections
-        street_before, street_after = geom_o.split_line_at_point(street_geom, point_veh[0])
-        edge_attr = {'geometry': street_before, 'length': street_before.length, 'is_veh_edge': True}
-        graph_iter.add_edge(node, street[0], attr_dict=edge_attr)
-        edge_attr = {'geometry': street_after, 'length': street_after.length, 'is_veh_edge': True}
-        graph_iter.add_edge(node, street[1], attr_dict=edge_attr)
-
-        # Copy the created graph
-        graphs_vehs[iteration] = graph_iter.copy()
-
-    vehs = Vehicles(points_vehs, graphs_vehs)
-    return vehs
 
 def main_sim(place, which_result=1, density_veh=100, use_pathloss=True, max_pl=150, density_type='absolute',
              debug=False):
@@ -158,12 +72,12 @@ def main_sim(place, which_result=1, density_veh=100, use_pathloss=True, max_pl=1
     else:
         raise ValueError('Density type not supported')
 
-    rand_street_idxs = dist.choose_random_streets(street_lengths, count_veh)
+    rand_street_idxs = vehicles.choose_random_streets(street_lengths, count_veh)
     utils.debug(debug, time_start)
 
     # Vehicle generation
     time_start = utils.debug(debug, None, 'Generating vehicles')
-    vehs = generate_vehs(graph_streets, rand_street_idxs)
+    vehs = vehicles.generate_vehs(graph_streets, rand_street_idxs)
     utils.debug(debug, time_start)
 
     # Find center vehicle
@@ -181,7 +95,7 @@ def main_sim(place, which_result=1, density_veh=100, use_pathloss=True, max_pl=1
 
     is_nlos = prop.veh_cons_are_nlos(vehs.get_points('center'),
                                      vehs.get_points('other'), gdf_buildings)
-    vehs.add_key('nlos', index_other_vehs[is_nlos])  
+    vehs.add_key('nlos', index_other_vehs[is_nlos])
     is_olos_los = np.invert(is_nlos)
     vehs.add_key('olos_los', index_other_vehs[is_olos_los])
 
@@ -194,7 +108,7 @@ def main_sim(place, which_result=1, density_veh=100, use_pathloss=True, max_pl=1
 
         # NOTE: A margin of 2, means round cars with radius 2 meters
         is_olos = prop.veh_cons_are_olos(vehs.get_points('center'),
-                                        vehs.get_points('olos_los'), margin=2)
+                                         vehs.get_points('olos_los'), margin=2)
         is_los = np.invert(is_olos)
         vehs.add_key('olos', vehs.get_idxs('olos_los')[is_olos])
         vehs.add_key('los', vehs.get_idxs('olos_los')[is_los])
@@ -206,9 +120,9 @@ def main_sim(place, which_result=1, density_veh=100, use_pathloss=True, max_pl=1
 
         is_orthogonal, coords_intersections = \
             prop.check_if_cons_orthogonal(streets_wave,
-                                        vehs.get_graph('center'),
-                                        vehs.get_graph('nlos'),
-                                        max_angle=np.pi)
+                                          vehs.get_graph('center'),
+                                          vehs.get_graph('nlos'),
+                                          max_angle=np.pi)
         is_paralell = np.invert(is_orthogonal)
         vehs.add_key('orth', vehs.get_idxs('nlos')[is_orthogonal])
         vehs.add_key('par', vehs.get_idxs('nlos')[is_paralell])
@@ -249,7 +163,7 @@ def main_sim(place, which_result=1, density_veh=100, use_pathloss=True, max_pl=1
         vehs.set_pathlosses('orth', pathlosses_orth)
         pathlosses_par = np.Infinity*np.ones(np.sum(is_paralell))
         vehs.set_pathlosses('par', pathlosses_par)
-        
+
         utils.debug(debug, time_start)
 
         # Determine in range / out of range
@@ -283,22 +197,7 @@ def main_sim(place, which_result=1, density_veh=100, use_pathloss=True, max_pl=1
     plt.show()
 
 
-
-
-def parse_arguments():
-    """Parses the command line arguments and returns them """
-    parser = argparse.ArgumentParser(description='Simulate vehicle connections on map')
-    parser.add_argument('-p', type=str, default='Neubau - Vienna - Austria', help='place')
-    parser.add_argument('-w', type=int, default=1, help='which result')
-    parser.add_argument('-d', type=float, default=1000, help='vehicle density')
-    parser.add_argument('-s', type=int, default=1, help='flag determining pathloss usage (instead of distance)')
-    parser.add_argument('-l', type=float, default=150, help='pathloss threshold [dB]')
-    parser.add_argument('-t', type=str, default='absolute',
-                        help='density type (absolute, length, area)')
-    arguments = parser.parse_args()
-    return arguments
-
 if __name__ == '__main__':
-    args = parse_arguments()
+    args = utils.parse_arguments()
     main_sim(args.p, which_result=args.w, density_veh=args.d, use_pathloss=bool(args.s), max_pl=args.l, density_type=args.t,
              debug=True)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 """ Various uncomplex functionality"""
 
 import time
+import argparse
 
 
 def string_to_filename(string):
@@ -30,3 +31,17 @@ def debug(is_debug_mode=False, time_start=None, text=None):
         time_diff = time.process_time() - time_start
         print_nnl(': {:.3f} seconds\n'.format(time_diff))
         return time_diff
+
+def parse_arguments():
+    """Parses the command line arguments and returns them """
+    parser = argparse.ArgumentParser(description='Simulate vehicle connections on map')
+    parser.add_argument('-p', type=str, default='Neubau - Vienna - Austria', help='place')
+    parser.add_argument('-w', type=int, default=1, help='which result')
+    parser.add_argument('-d', type=float, default=1000, help='vehicle density')
+    parser.add_argument('-s', type=int, default=1,
+                        help='use pathloss for connections (not distance)')
+    parser.add_argument('-l', type=float, default=150, help='pathloss threshold [dB]')
+    parser.add_argument('-t', type=str, default='absolute',
+                        help='density type (absolute, length, area)')
+    arguments = parser.parse_args()
+    return arguments
