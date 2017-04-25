be30148af4ed1234f34d71ed785016d283f3357c
Thomas Blazek <wtblazek@gmail.com>
2017-04-25 13:32:31 +0200

    pool

--- main_sim_distribution.py
+++ main_sim_distribution.py
@@ -6,7 +6,7 @@ import os.path
 import multiprocessing as mp
 from itertools import repeat
 import pickle
-
+from itertools import repeat
 
 # Extension imports
 import numpy as np
@@ -200,63 +200,59 @@ def main_sim_multi(network, max_dist_olos_los=250, max_dist_nlos=140, debug=Fals
     return net_connectivity, path_redundancy
 
 
-def main_sim(network, max_pl=150, debug=False, max_dist_olos_los=300,):
+def main_sim(network, max_pl=150, debug=False, max_dist_olos_los=300,center_veh=0):
     """Simulates the connections from one to all other vehicles using pathloss functions """
-
+    idx = center_veh
     # Initialize
     vehs=network['vehs']
     graph_streets_wave=network['graph_streets_wave']
     gdf_buildings=network['gdf_buildings']
     count_veh=vehs.count
     vehs.allocate(count_veh)
-    print("There are {} vehicles in the simulation".format(count_veh))
     # Find center vehicle
     # time_start = # utils.debug(debug, None, 'Finding center vehicle')
-    target_array=[]
-    distance_array=[]
-    for idx in range(count_veh):
-        print("\t Current Run: {}".format(idx))
-        idx_center_veh=idx
-        idxs_other_vehs=np.where(np.arange(count_veh) != idx_center_veh)[0]
-        vehs.add_key('center', idx_center_veh)
-        vehs.add_key('other', idxs_other_vehs)
-        # utils.debug(debug, # time_start)
 
-        # Determine propagation conditions
-        # time_start = # utils.debug(
-        #    debug, None, 'Determining propagation conditions')
-        is_nlos=prop.veh_cons_are_nlos(vehs.get_points('center'),
-                                         vehs.get_points('other'),
-                                         gdf_buildings,
-                                         max_dist=max_dist_olos_los)
-        vehs.add_key('nlos', idxs_other_vehs[is_nlos])
-        is_olos_los=np.invert(is_nlos)
-        vehs.add_key('olos_los', idxs_other_vehs[is_olos_los])
-        # utils.debug(debug, # time_start)
+    print("\t Current Run: {}/{}".format(idx,count_veh))
+    idx_center_veh=center_veh
+    idxs_other_vehs=np.where(np.arange(count_veh) != idx_center_veh)[0]
+    vehs.add_key('center', idx_center_veh)
+    vehs.add_key('other', idxs_other_vehs)
+    # utils.debug(debug, # time_start)
 
-        # Determine OLOS and LOS
-        # time_start = # utils.debug(debug, None, 'Determining OLOS and LOS')
-        # NOTE: A margin of 2, means round cars with radius 2 meters
-        is_olos=prop.veh_cons_are_olos(vehs.get_points('center'),
-                                         vehs.get_points('olos_los'), margin=2)
-        is_los=np.invert(is_olos)
-        vehs.add_key('olos', vehs.get_idxs('olos_los')[is_olos])
-        vehs.add_key('los', vehs.get_idxs('olos_los')[is_los])
-        # utils.debug(debug, # time_start)
-        d=min([vehs.get_points('center').distance(geo.Point(x[1]['x'],
-                                                            x[1]['y']))
-               for x in graph_streets_wave.node.items()])
-        target_array.append(len(vehs.get_idxs("olos_los")))
-        distance_array.append(d)
-        print((target_array, [int(x) for x in distance_array]))
-        with open("target_array.p", "wb") as fp:
-            pickle.dump((target_array, distance_array), fp)
+    # Determine propagation conditions
+    # time_start = # utils.debug(
+    #    debug, None, 'Determining propagation conditions')
+    is_nlos=prop.veh_cons_are_nlos(vehs.get_points('center'),
+                                        vehs.get_points('other'),
+                                        gdf_buildings,
+                                        max_dist=max_dist_olos_los)
+    vehs.add_key('nlos', idxs_other_vehs[is_nlos])
+    is_olos_los=np.invert(is_nlos)
+    vehs.add_key('olos_los', idxs_other_vehs[is_olos_los])
+    # utils.debug(debug, # time_start)
+
+    # Determine OLOS and LOS
+    # time_start = # utils.debug(debug, None, 'Determining OLOS and LOS')
+    # NOTE: A margin of 2, means round cars with radius 2 meters
+    is_olos=prop.veh_cons_are_olos(vehs.get_points('center'),
+                                        vehs.get_points('olos_los'), margin=2)
+    is_los=np.invert(is_olos)
+    vehs.add_key('olos', vehs.get_idxs('olos_los')[is_olos])
+    vehs.add_key('los', vehs.get_idxs('olos_los')[is_los])
+    # utils.debug(debug, # time_start)
+    d=min([vehs.get_points('center').distance(geo.Point(x[1]['x'],
+                                                        x[1]['y']))
+            for x in graph_streets_wave.node.items()])
+    return (len(vehs.get_idxs("olos_los"))), (d)
+
+       
 
 
 def execute_main():
     sim_mode='single'  # 'single', 'multi', 'multiprocess'
     place='Upper West Side - New York - USA'
     place='Linz Stadt - Austria'
+    place='Neubau - Vienna - Austria'
     use_pathloss=False  # TODO: Implement functions to use use_pathloss
     which_result=1
     densities_veh=50e-3
@@ -320,7 +316,12 @@ def execute_main():
         generate_vehicles(net, density_veh=densities_veh,
                           density_type=static_params['density_type'],
                           debug=False)
-        main_sim(net, max_pl=max_pl, debug=True)
+        with mp.Pool() as p:
+            return_val = p.starmap(main_sim, zip(repeat(net),repeat(max_pl),
+                                               repeat(True), repeat(300), range(net['vehs']  .count)))
+        with open("return_vals.p","wb") as fp:
+            pickle.dump(return_val, fp)
+        
 
     else:
         raise NotImplementedError('Simulation type not supported')
