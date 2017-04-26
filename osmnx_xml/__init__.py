163d8fb8340f9f213fdea320888e7822d6650fb1
Dosenpfand <m@sad.bz>
2017-04-26 17:08:39 +0200

    add osmnx source

---
+++ osmnx_xml/__init__.py
@@ -0,0 +1,18 @@
+###################################################################################################
+# Module: __init__.py
+# Description: OSMnx: Retrieve, construct, analyze, and visualize street networks from OSM
+# License: MIT, see full license in LICENSE.txt
+# Web: https://github.com/gboeing/osmnx
+###################################################################################################
+
+from .buildings import *
+from .elevation import *
+from .core import *
+from .plot import *
+from .projection import *
+from .save_load import *
+from .simplify import *
+from .stats import *
+from .utils import *
+
+__version__ = '0.5'
