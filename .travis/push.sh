1957fbfbd70fa709cb63159ad2eefa2284f9ef20
Dosenpfand <m@sad.bz>
2017-05-14 13:23:27 +0200

    add force push

--- .travis/push.sh
+++ .travis/push.sh
@@ -13,7 +13,7 @@ commit_coverage_image() {
 
 push_files() {
   git remote add origin-travis https://${GH_TOKEN}@github.com/Dosenpfand/thesis_code.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-travis travis
+  git push -f --quiet --set-upstream origin-travis travis
 }
 
 setup_git
