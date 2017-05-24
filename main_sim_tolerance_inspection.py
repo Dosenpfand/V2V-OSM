6f5a0ab42e433797f8c2315ce3c67bba287743ef
Dosenpfand <m@sad.bz>
2017-05-24 15:31:42 +0200

    adapt analyze tolerance for multisim

--- main_sim_tolerance_inspection.py
+++ main_sim_tolerance_inspection.py
@@ -8,41 +8,51 @@ import numpy as np
 import main_sim
 import utils
 import json
+import network_parser as nw_p
 
 
-def analyze_tolerance():
+def analyze_tolerance(conf_path):
     """Analyzes the simulation results by comparing connection matrices from simulations with and without tolerance"""
 
-    counts_vehs = [10, 100, 1000]
+    config = nw_p.params_from_conf(config_file=conf_path)
+    # TODO: support ranges and other types of vehicle densities
+    counts_vehs = config['densities_veh']
 
-    results = []
+    scenarios = nw_p.get_scenarios_list(conf_path)
+    suffixes = set()
+    for scenario in scenarios:
+        suffixes.add(scenario[12:])
 
-    for count_vehs in counts_vehs:
-        res_wo = utils.load('results/tolerance_test_without.{:d}.pickle.gz'.format(count_vehs))
-        res_w = utils.load('results/tolerance_test_with.{:d}.pickle.gz'.format(count_vehs))
-        run_time_wo = res_wo['info']['time_finish'] - res_wo['info']['time_start']
-        run_time_w = res_w['info']['time_finish'] - res_w['info']['time_start']
-        matrices_cons_wo = res_wo['results']['matrices_cons']
-        matrices_cons_w = res_w['results']['matrices_cons']
 
-        count_diff = 0
-        count_tot = 0
+    for suffix in list(suffixes):
 
-        for matrix_cons_wo, matrix_cons_w in zip(matrices_cons_wo, matrices_cons_w):
-            count_diff += np.nonzero(matrix_cons_wo != matrix_cons_w)[0].size
-            count_tot += matrix_cons_w.size
+        results = []
 
-        ratio_diff = count_diff / count_tot
-        result = {'count_vehs': count_vehs,
-                  'count_con_tot': count_tot,
-                  'count_con_diff': count_diff,
-                  'ratio_con_diff': ratio_diff,
-                  'run_time_wo': run_time_wo,
-                  'run_time_w': run_time_w}
-        results.append(result)
+        for count_vehs in counts_vehs:
+            res_wo = utils.load('results/tolerance_0_{}.{:d}.pickle.gz'.format(suffix, count_vehs))
+            res_w = utils.load('results/tolerance_1_{}.{:d}.pickle.gz'.format(suffix, count_vehs))
+            run_time_wo = res_wo['info']['time_finish'] - res_wo['info']['time_start']
+            run_time_w = res_w['info']['time_finish'] - res_w['info']['time_start']
+            matrices_cons_wo = res_wo['results']['matrices_cons']
+            matrices_cons_w = res_w['results']['matrices_cons']
 
-    utils.save(results, 'results/tolerance_test_comparison')
-    return results
+            count_diff = 0
+            count_tot = 0
+
+            for matrix_cons_wo, matrix_cons_w in zip(matrices_cons_wo, matrices_cons_w):
+                count_diff += np.nonzero(matrix_cons_wo != matrix_cons_w)[0].size
+                count_tot += matrix_cons_w.size
+
+            ratio_diff = count_diff / count_tot
+            result = {'count_vehs': count_vehs,
+                      'count_con_tot': count_tot,
+                      'count_con_diff': count_diff,
+                      'ratio_con_diff': ratio_diff,
+                      'run_time_wo': run_time_wo,
+                      'run_time_w': run_time_w}
+            results.append(result)
+
+        utils.save(results, 'results/tolerance_comparison_{}.pickle.gz'.format(suffix))
 
 
 if __name__ == '__main__':
@@ -58,5 +68,5 @@ if __name__ == '__main__':
 
     main_sim.main_multi_scenario(conf_path=config_file_path)
 
-    results = analyze_tolerance()
+    results = analyze_tolerance(config_file_path)
     print(results)
