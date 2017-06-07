6eba49eb1f4d20b930931164302178d7633a95cf
Dosenpfand <m@sad.bz>
2017-06-07 15:00:19 +0200

    expand docstring for utils

--- vtovosm/utils.py
+++ vtovosm/utils.py
@@ -1,4 +1,4 @@
-""" Various uncomplex functionality"""
+"""Various functionality that does not fit in any other module."""
 
 import datetime
 import getpass
@@ -17,7 +17,18 @@ import scipy.stats as st
 
 
 def string_to_filename(string):
-    """Cleans a string up to be used as a filename"""
+    """Returns a cleaned up string that can be used as a filename.
+
+    Parameters
+    ----------
+    string : str
+        String that will be cleaned up.
+
+    Returns
+    -------
+    filename: str
+        Cleaned up string
+    """
 
     keepcharacters = ('_', '-')
     filename = ''.join(c for c in string if c.isalnum()
@@ -27,7 +38,18 @@ def string_to_filename(string):
 
 
 def seconds_to_string(seconds):
-    """Converts an amount of seconds to a dd:hh:mm:ss string"""
+    """Converts an amount of seconds to a a string with format "dd:hh:mm:ss".
+
+    Parameters
+    ----------
+    seconds : int or float
+        Number of seconds that will be converted
+
+    Returns
+    -------
+    string : str
+        Formatted string
+    """
 
     dtime = datetime.datetime(
         1, 1, 1) + datetime.timedelta(seconds=int(seconds))
@@ -42,13 +64,32 @@ def seconds_to_string(seconds):
 
 
 def print_nnl(text, file=sys.stdout):
-    """Print without adding a new line """
+    """Print without adding a new line.
+
+    Parameters
+    ----------
+    text : str
+        Text to be printed
+    file: optional
+        file object to which will be printed
+    """
 
     print(text, file=file, end='', flush=True)
 
 
 def debug(time_start=None, text=None):
-    """ Times execution and outputs log messages"""
+    """Times execution and outputs log messages.
+    If `time_start` is None, it will be interpreted as a start message and `text` will be logged.
+    If `time_start` is None, it will be interpreted as start time and `text` will be logged together with the time
+    difference between now and `time_start`.
+
+    Parameters
+    ----------
+    time_start : float, optional
+        Start time of the corresponding action.
+    text : str, optional
+        Text that will be logged
+    """
 
     if time_start is None:
         if text is not None:
@@ -62,8 +103,27 @@ def debug(time_start=None, text=None):
 
 
 def square_to_condensed(idx_i, idx_j, size_n):
-    """Converts the squareform indices i and j of the square matrix with with size n x n to the
-    condensed index k. See also: scipy.spatial.distance.squareform"""
+    """Converts the squareform indices i and j of the square matrix with with size `size_n` x `size_n` to the
+    condensed index k.
+
+    Parameters
+    ----------
+    idx_i : int
+        Row index of the square matrix
+    idx_j : int
+        Column index of the square matrix
+    size_n :
+        Size of the square matrix
+
+    Returns
+    -------
+    k : int
+        Index of the condensed vector
+
+    See Also
+    --------
+    scipy.spatial.distance.squareform
+    """
 
     if idx_i == idx_j:
         raise ValueError('Diagonal entries are not defined')
@@ -74,8 +134,27 @@ def square_to_condensed(idx_i, idx_j, size_n):
 
 
 def condensed_to_square(index_k, size_n):
-    """Converts the condensed index k of the condensed vector with size n to the square indicies i
-    and j. See also scipy.spatial.distance.squareform"""
+    """Converts the condensed index k of the condensed vector to the indicies i and j of the square matrix with
+    size `size_n` x `size_n`.
+
+    Parameters
+    ----------
+    index_k : int
+        Index of the condensed vector
+    size_n : int
+        Size of the square matrix
+
+    Returns
+    -------
+    i : int
+        Row index of the square matrix
+    j : int
+        Column index of the square matrix
+
+    See Also
+    --------
+    scipy.spatial.distance.squareform
+    """
 
     def calc_row_idx(index_k, size_n):
         """Determines the row index"""
@@ -99,7 +178,23 @@ def condensed_to_square(index_k, size_n):
 
 
 def net_connectivity_stats(net_connectivities, confidence=0.95):
-    """Calculates the means and confidence intervals for network connectivity results"""
+    """Calculates the means and confidence intervals for network connectivity results.
+
+    Parameters
+    ----------
+    net_connectivities : list of float
+        Network connectivities
+    confidence : float, optional
+        Confidence interval
+
+    Returns
+    -------
+    means : array of float
+        Means
+    conf_intervals : array of float
+        Confidence intervals
+
+    """
 
     means = np.mean(net_connectivities, axis=0)
     conf_intervals = np.zeros([np.size(means), 2])
@@ -112,7 +207,15 @@ def net_connectivity_stats(net_connectivities, confidence=0.95):
 
 
 def send_mail_finish(recipient=None, time_start=None):
-    """Sends an email to notify someone about the finished simulation"""
+    """Sends an email to notify someone about the finished simulation using a local mail server.
+
+    Parameters
+    ----------
+    recipient : str, optional
+        recipient of the email. If it is `None` than it will be sent to the user executing python.
+    time_start: float, optional
+        Time at which the simulation has been started.
+    """
 
     if time_start is None:
         msg = MIMEText('The simulation is finished.')
@@ -137,7 +240,23 @@ def send_mail_finish(recipient=None, time_start=None):
 
 
 def save(obj, file_path, protocol=4, compression_level=1, overwrite=True, create_dir=True):
-    """Saves an object using gzip compression"""
+    """Saves an object using gzip compression.
+
+    Parameters
+    ----------
+    obj :
+        Object that will be saved
+    file_path : str
+        Path at which `obj` will be saved
+    protocol : int, optinal
+        Gzip protocol
+    compression_level : int, optional
+        Gzip compression level
+    overwrite : bool, optional
+        When true an already existing file will be overwritten.
+    create_dir : bool, optional
+        When true any non existing intermediary directories in `file_path` will be created.
+    """
 
     # Return if file already exists
     if not overwrite and os.path.isfile(file_path):
@@ -154,14 +273,37 @@ def save(obj, file_path, protocol=4, compression_level=1, overwrite=True, create
 
 
 def load(file_path):
-    """Loads and decompresses a saved object"""
+    """Loads and decompresses a saved object.
+
+    Parameters
+    ----------
+    file_path : str
+        Path of the compressed file
+
+    Returns
+    -------
+    object
+        Object that was saved in the file
+    """
 
     with gzip.open(file_path, 'rb') as file:
         return pickle.load(file)
 
 
 def compress_file(file_in_path, protocol=4, compression_level=1, delete_uncompressed=True):
-    """Loads an uncompressed file and saves a compressed copy of it"""
+    """Loads an uncompressed file and saves a compressed copy of it
+
+    Parameters
+    ----------
+    file_in_path : str
+        Path of the uncompressed file
+    protocol : int, optinal
+        Gzip protocol version
+    compression_level : int, optional
+        Gzip compression level
+    delete_uncompressed : bool, optional
+        When True, the uncompressed file will be deleted after compression
+    """
 
     file_out_path = file_in_path + '.gz'
     with open(file_in_path, 'rb') as file_in:
